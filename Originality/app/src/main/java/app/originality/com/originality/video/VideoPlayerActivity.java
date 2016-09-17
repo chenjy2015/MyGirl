b6aa317ca96945a834da7c14d5d2ffdc600a1e20
chenjy2015 <63589201@qq.com>
2016-09-17 17:15:07 +0800

    新增视频播放Activity

---
+++ Originality/app/src/main/java/app/originality/com/originality/video/VideoPlayerActivity.java
@@ -0,0 +1,13 @@
+package app.originality.com.originality.video;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VideoPlayerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+    }
+}
